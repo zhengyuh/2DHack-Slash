50d0c44f5e027eb72ca5aab1c2dd03120e2f0404
Zhengyuan Huang <zhengyuh@uci.edu>
2016-09-26 22:12:27 -0700

    First Commit
    Added Unity project to GitHub.

---
+++ 2DHackNSlash/Assets/Scripts/CameraAspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAspector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.aspect = 1920f / 1080f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 2DHackNSlash/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() {
+        Vector2 moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(moveVector != Vector2.zero) {
+            anim.SetBool("IsWalking", true);
+            anim.SetFloat("Input_X", moveVector.x);
+            anim.SetFloat("Input_Y", moveVector.y);
+            anim.speed = speed / 5;
+        } else {
+            anim.SetBool("IsWalking", false);
+        }
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+    }
+}
